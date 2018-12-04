ce9437e5332cd042a871ee24140d789dea1ff581
Prestamo <prestamo@hnsc.net>
2018-12-04 11:40:11 -0600

    First Commit

--- src/components/hero/heroComponent.js
+++ src/components/hero/heroComponent.js
@@ -2,13 +2,36 @@ import React, { Component } from 'react';
 import '../../styles/my-theme.css';
 
 class HeroComponent extends Component {
+constructor(props){
+  super(props);
+  this.state = {
+    name : 'Julio Zúniga',
+    job : 'System Administrator',
+    title : '¡Hello World!'
+  }
+}
+
+myFunction(){
+  alert('Hello World!');
+}
+
   render() {
     return (
-      <section className="main-section hero-header section--with-backdrop section--with-parallax">
-        <h2 className="hero-header__subtitle">Hello there! I am</h2>
-        <h1 className="hero-header__title">Ramon Calix</h1>
-        <h2 className="hero-header__subtitle">Web developer, HN</h2>
-    </section>
+      <div>
+        
+        <section className="main-section hero-header section--with-backdrop section--with-parallax">
+          <h2 className="hero-header__subtitle">{this.state.title}</h2>
+          <h1 className="hero-header__title">{this.state.name}</h1>
+          <h2 className="hero-header__subtitle">{this.state.job}</h2>
+        </section>
+        
+            <div>
+              <button type='button' onClick={() => this.myFunction()}>My Button</button>
+            </div>
+
+      </div>
+
+
     );
   }
 }
